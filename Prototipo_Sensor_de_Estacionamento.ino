b13f566ed2055e30d1ee98565b5275e7ae6f8e9e
PontoCursos <emersoncarvalho@outlook.com>
2019-04-15 15:08:36 -0300

    Add files via upload

---
+++ Prototipo_Sensor_de_Estacionamento.ino
@@ -0,0 +1,82 @@
+/******************************************************************************/
+/*          Projeto: Protótipo Sensor para Estacionamento em Garagem          */
+/*                     PontoCanal - Ponto Makers                              */
+/******************************************************************************/
+
+//Configuração de Distancia Mínima em centimetros
+const int distancia_carro = 10; 
+
+//Configurações de Portas do Arduino
+
+//Sensor
+const int TRIG = 3;
+const int ECHO = 2;
+
+//Demais componentes
+const int ledGreen = 7;
+const int ledRed = 8;
+const int buzzer = 9;
+
+// Variaveis para funcionamento do Buzzer
+float seno;
+int frequencia;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Configurações do Sensor
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+  
+  // Configurações do LED
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+
+  //Configurações do Buzzer
+  pinMode(buzzer, OUTPUT); 
+  
+}
+
+void loop() {
+  int distancia = sensor_morcego(TRIG,ECHO);
+
+    if(distancia <= distancia_carro){
+      Serial.print("Atenção: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledRed, HIGH);
+      tocaBuzzer();
+    }
+    else{
+      Serial.print("Livre: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+      digitalWrite(ledGreen, HIGH);
+      digitalWrite(ledRed, LOW);
+      noTone(buzzer);
+    }
+  delay(100);
+  
+}
+
+int sensor_morcego(int pinotrig,int pinoecho){
+  digitalWrite(pinotrig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinotrig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinotrig,LOW);
+
+  return pulseIn(pinoecho,HIGH)/58;
+}
+
+//Função para execução do Alarme Sonoro
+void tocaBuzzer(){
+ for(int x=0;x<180;x++){
+  seno=(sin(x*3.1416/180));
+  frequencia = 2000+(int(seno*1000));
+  tone(buzzer,frequencia);
+  delay(2);
+}
+
+}
